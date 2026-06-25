cd8fbba3d0e8cb02ea548778728e6c4dbad0e18a
suku <sukumarreddyp06@gmail.com>
2026-06-25 17:29:42 +0530

    Merge remote changes

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/facilities")
 def facilities():
     return render_template("facilities.html")
-
 
 @app.route("/pricing")
 def pricing():
     return render_template("pricing.html")
 
-
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html")
-
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
